03e76131e4c68dbb858daae0bef6e468072b520f
bruno.falcao <brunocesar.sf.16@gmail.com>
2025-12-01 03:44:25 -0400

    initial commit | SERVER

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+require('dotenv').config({ path: '../.env' })  
+const express = require("express");
+const app = express();
+const port = process.env.SRVR_PORT;            
+app.use(express.json());
+
+app.get("/", function(req, res) {
+    res.send("Server is up")
+})
+
+app.listen(port, () => {
+    console.log(`server running on port ${port}`);
+});
